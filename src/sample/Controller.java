fdc4f2d46681b29b55b9d50601be0a85edd949c1
dejaan <dpopovic1@etf.unsa.ba>
2019-03-25 12:53:47 +0100

    prva verzija

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    public Label ispis;
+    public TextField unos;
+
+    public void prepravi(ActionEvent actionEvent) {
+        String str = unos.getText(), najduza = "", rezultat = "";
+        for(String rijec: str.split(" ")) {
+            if(rijec.length() > najduza.length()) najduza = rijec;
+        }
+        for(int i=0; i<najduza.length(); i++) {
+
+            if(Character.isLowerCase(najduza.charAt(i))) rezultat += Character.toUpperCase(najduza.charAt(i));
+        }
+        ispis.setText(najduza);
+    }
 }
